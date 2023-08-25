53c9bf576e7479f43545f7c511035e4c66e739da
Muhammad Waseem <48334073+wa8eem@users.noreply.github.com>
2023-08-25 09:44:19 +0200

    Made it just plain simple!

--- restartmpv.lua
+++ restartmpv.lua
@@ -1,19 +1,10 @@
 local utils = require 'mp.utils'
-
-local last_file_path = nil
-
 function restart_mpv()
     mp.command_native({"quit"})
     
-    if last_file_path then
-        local args = {}
-        args.args = {"mpv", last_file_path}
-        utils.subprocess_detached(args)
-    end
+    local args = {}
+    args.args = {"mpv"}
+    
+    utils.subprocess_detached(args)
 end
-
-mp.observe_property("path", "string", function(_, path)
-    last_file_path = path
-end)
-
 mp.add_key_binding("", "restart-mpv", restart_mpv)
